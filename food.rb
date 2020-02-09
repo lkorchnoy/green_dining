d9b6926423661c94392ece13a518753669861101
lkorchnoy <55862411+lkorchnoy@users.noreply.github.com>
2020-02-09 18:55:48 -0500

    Add files via upload

---
+++ food.rb
@@ -0,0 +1,42 @@
+class Food
+extend Concerns::Findable
+
+attr_accessor :name, :zip_code
+
+@@all = []
+
+def initialize(name)
+    @name = name
+    @zip_code = []
+end
+
+def self.all
+    @@all
+end
+
+def self.destroy_all
+    @@all.clear
+end
+
+def self.create
+    create.new.save
+end
+
+def add_zip_code(zipcode)
+    @zip_code << zipcode unless @zip_code.include?(zipcode)
+end
+
+def person
+    self.zip_code.collect {|z| z.person == food}.uniq
+    person_get_food
+    person_give_food
+end
+
+def save 
+    @@all << self
+end
+
+
+
+
+end
---
+++ version.rb
@@ -0,0 +1,4 @@
+
+module GreenDining
+  VERSION = "0.1.0"
+end
