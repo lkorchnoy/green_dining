3793aaddc0e9f13fbe78ca1ab24b3ceae9294ee8
lkorchnoy <lkorchnoy@yahoo.com>
2020-02-10 16:19:02 -0500

    fix scrapper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,13 +8,15 @@ class GreenDining::Scraper
     #def get_page
         
 
-    def self.scrape.dining 
-        doc = Nokogiri::HTML(open("https://jbjsoulkitchen.org))
+    def self.scrape_dining 
+        doc = Nokogiri::HTML(open("https://jbjsoulkitchen.org"))
         binding.pry
             dining_event = self.new
             dining.main = doc.search("h2.et_pb_slide_title").text 
             dining.menu_red_bank = doc.search("article.post-1229").text
             dining.menu_toms_river = doc.search("div.entry-content").text
     end
+end
+
 
                 
