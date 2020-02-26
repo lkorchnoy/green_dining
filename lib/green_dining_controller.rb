483cd73a650cf75737989e6e9a63fb0498e85d7c
lkorchnoy <lkorchnoy@yahoo.com>
2020-02-25 22:28:33 -0500

    final improvement

--- lib/green_dining_controller.rb
+++ lib/green_dining_controller.rb
@@ -1,87 +1,78 @@
-     require 'pry'
-
-class GreenDining::GreenDiningController
+    class GreenDining::GreenDiningController
 
        def call
-        main_menu
-        user_input
-        what_next
+          main_menu
+          user_input
        end
 
-        def main_menu
-        puts "Welcome to the Green Dining App! Discover community dining and food sharing experience."
-        puts "Share food Not waste food"
-        puts "Here are the 2 locations."
-        puts "1. JBJ Soul Kitchen Red Bank location"
-        puts "2. JBJ Soul Kitchen Toms River location"
-        puts "To select, please enter '1' or '2'."
-        puts "To go back to the main menu, enter 'menu'."
-        puts "To exit, please type 'exit'."
-               
-        puts "What would you like to do?"
+       def main_menu
+          puts "Welcome to the Green Dining App! Discover community dining and food sharing experience."
+          puts "Share food Not waste food"
+          puts "Here are the 2 locations."
+          puts "1. JBJ Soul Kitchen Red Bank location"
+          puts "2. JBJ Soul Kitchen Toms River location"
+          puts "To select, please enter '1' or '2'."
+          puts "To go back to the main menu, enter 'menu'."
+          puts "To exit, please type 'exit'."
+                
+          puts "What would you like to do?"
           user_input
         end
 
-        def return_home
-          puts "To exit the app, enter 'exit'."
-          puts "To return to the main menu, enter 'menu'."
-          @input = gets.strip.downcase
-          if @input == 'exit'
+       def return_home
+           puts "To exit the app, enter 'exit'."
+           puts "To return to the main menu, enter 'menu'."
+           @input = gets.strip.downcase
+         if @input == 'exit'
             goodbye
-          elsif @input == 'menu'
+         elsif @input == 'menu'
             main_menu
-          else 
-            puts "Try again"
+         else 
+           puts "Try again"
             return_home
           
+         end
         end
-      end
 
         def user_input
-            @input = nil
-            while @input != 'exit'
-            #@input = gets.strip.downcase
-            user_input_one
-            user_input_two
-            
+           @input = nil
+         while @input != 'exit'
+           user_input_one
+           user_input_two
           end 
-        end
+         end
 
-         def user_input_one
-              @input = gets.strip
-              if @input.to_i == 1
+        def user_input_one
+           @input = gets.strip
+         if @input.to_i == 1
               
-              puts "Great choice. Welcome to Red Bank Green Dining"
-              @current_location = GreenDining::Scraper.scrape_redbank_location
-              @current_menu = GreenDining::Scraper.scrape_redbank_menu
-              @redbank = GreenDining::GreenDining.new(@current_location, @current_menu)
-              puts @redbank.location
+            puts "Great choice. Welcome to Red Bank Green Dining"
+            @current_location = GreenDining::Scraper.scrape_redbank_location
+            @current_menu = GreenDining::Scraper.scrape_redbank_menu
+            @redbank = GreenDining::GreenDining.new(@current_location, @current_menu)
+            puts @redbank.location
              
-              puts "Would you like to see the menu for the current location?"
-              puts "Type 'y' to see the menu"
-              @input = gets.strip.downcase
-              if @input == 'y'
-              puts @redbank.menu
-               return_home
-              #@input = gets.strip.downcase
-              #elsif @input == 'menu'
-                #main_menu
-              #elsif @input == 'exit'
-                #goodbye
-              else 
-             puts "Invalid input, please try again"
-               main_menu
+            puts "Would you like to see the menu for the current location?"
+            puts "Type 'y' to see the menu"
+            @input = gets.strip.downcase
+          if @input == 'y'
+            puts @redbank.menu
+              return_home
+          else 
+            puts "Invalid input, please try again"
+              main_menu
              end
-             @input = gets.strip.downcase
-             elsif @input == 'exit'
-              goodbye
-            end
-            end
+             
+          elsif @input == 'menu'
+             main_menu
+          elsif @input == 'exit'
+            goodbye
+           end
+          end
              
             
-            def user_input_two
-            @input = gets.strip
-            if @input.to_i == 2 
+        def user_input_two
+          if @input.to_i == 2 
             puts "Great choice. Welcome to Toms River Dining"
             @current_location = GreenDining::Scraper.scrape_tomsriver_location
             @current_menu = GreenDining::Scraper.scrape_tomsriver_menu
@@ -91,37 +82,26 @@ class GreenDining::GreenDiningController
             puts "Would you like to see the menu for the current location?"
             puts "Type 'y' to see the menu"
             @input = gets.strip.downcase
-              if @input == 'y'
-                  puts @tomsriver.menu
-                  return_home
-                  #@input = gets.strip.downcase
-                  #elsif @input == 'menu'
-                  #main_menu
-                  #elsif @input == 'exit'
-                    #goodbye
-                  else 
+          if @input == 'y'
+            puts @tomsriver.menu
+             return_home
+          else 
             puts "Invalid input, please try again"
              main_menu
              end
-             @input = gets.strip.downcase
-             elsif @input == 'exit'
-              goodbye
+             
+          elsif @input == 'menu'
+            main_menu
+          elsif @input == 'exit'
+            goodbye
             end
         end
 
 
             
-          def what_next
-          @input = gets.strip.downcase
-          puts "Are you done? Type 'exit' to exit"
-          puts "To return to the main menu, enter 'menu'."
-          main_menu
-        end 
-        
-        
-         def goodbye
+        def goodbye
           puts "Enjoy Green Dining!"
           exit
-         end
+        end
 
-      end   
+       end   
